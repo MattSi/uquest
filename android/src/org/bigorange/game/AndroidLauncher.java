05260b7a601c6f671824df33de39c548d611b225
propig <mian.si@outlook.com>
2022-03-20 14:23:31 +0800

    init.

---
+++ android/src/org/bigorange/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.bigorange.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.bigorange.game.UndergroundQuest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UndergroundQuest(), config);
+	}
+}
---
+++ desktop/src/org/bigorange/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package org.bigorange.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.bigorange.game.UndergroundQuest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+		config.width = 1280;
+		new LwjglApplication(new UndergroundQuest(), config);
+	}
+}
